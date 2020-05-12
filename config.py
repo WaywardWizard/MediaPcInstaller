127cff386560fcab131c2a4453a7da806ad19fce
Ben Tomlin <obscured@>
2020-05-13 11:58:03 +1200

    config

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 openelecimage='OpenELEC-Generic.x86_64-8.0.4.img'
 lakkaimage='Lakka-Generic.x86_64-2.3.2.img'
 repopath='/home/ben/eclipse-workspace/MediaPcInstaller/'
@@ -6,41 +7,55 @@ config = {
         'openelec':{
             'tarballurl':'http://releases.openelec.tv/{}.gz'.format(openelecimage),
             'tarballpath':'{}{}.gz'.format(repopath,openelecimage),
-            'bootloader':{
-                'kernelArgument':'ssh quiet'
-                },
+            'bootloader':[
+                {'OpenELEC':{
+                    'karg':'ssh quiet',
+                    'kpath':'/KERNEL',
+                    'class':'kodi'
+                    }},
+                ],
             'partition':{
                 'system':{
                     'size':1*1024,
                     'fslabel':'oe_system_root',
-                    'partitionlabel':'oe_root'
+                    'partitionlabel':'oe_root',
+                    'fstype':'ext4',
                     },
                 'data':{
                     'size':12*1024,
                     'fslabel':'oe_data',
-                    'partitionlabel':'oe_data'
+                    'partitionlabel':'oe_data',
+                    'fstype':'ext4',
                     },
                 },
             },
         'lakka':{
             'tarballurl':'http://le.builds.lakka.tv/Generic.x86_64/{}.gz'.format(lakkaimage),
             'tarballpath':'{}{}.gz'.format(repopath,lakkaimage),
-            'bootloader':{
-                'entries':[
-                    ]
+            'bootloader':[
+                {'Lakka':{
+                    'karg':'ssh quiet',
+                    'kpath':'/KERNEL',
+                    'class':'lakka'
+                    }
                 },
+                ],
             'partition':{
                 'system':{
                     'size':1*1024,
                     'fslabel':'lakka_system_root',
                     'partitionlabel':'lakka_root',
+                    'fstype':'ext4',
                     },
                 'data':{
                     'size':36*1024,
                     'fslabel':'lakka_data',
                     'partitionlabel':'lakka_data',
+                    'fstype':'ext4',
                     },
                 },
             }
         },
     }   
+
+ROOT=Path(__file__).parent
