f1b709f39d82a2298ec8075e06d983f04f9f7332
Ben Tomlin <obscured@>
2020-05-13 11:59:42 +1200

    Update installer

--- install.py
+++ install.py
@@ -1,50 +1,92 @@
-from disk import GrubDisk
+from grub.grub import GrubDisk
 from config import config
 import json
 from pathlib import Path
 from util import bash,dlFile
 import tempfile
 import shutil
+import os
 
 dev=Path('/dev/sdb')
 
 disk = GrubDisk(dev)
 
-for os,data in config['os'].items():
-    for ptn,info in data['partition']:
-        disk.addPartition(info['size'], info['partitionlabel'],info['fslabel'])
+for osid,data in config['os'].items():
+    for ptn,info in data['partition'].items():
+        uuid=disk.partitiontable.addPartition(info['size'], info['partitionlabel'],fslabel=info['fslabel'],fstype=info['fstype'])
+        info['uuid']=uuid
         
-disk.writePartitionTable()
-disk.mkfsOnPartitions()
 
-workingDirectory=Path(tempfile.mkdtemp())
-
-def getFirstPartitionOffset(file):
-    data=json.loads(bash('sfdisk -l {} -J'.format(file)).stdout)
+def getFirstPartitionOffset(imageFile):
+    data=json.loads(bash('sfdisk -l {} -J'.format(imageFile)).stdout)
     sectorsize=data['partitiontable']['sectorsize']
     firstsector=min([n['start'] for n in data['partitiontable']['partitions']])
     return(sectorsize*firstsector)
 
-for os in config['os'].keys:
-    osdata=config['os'][os]
-    ostarball=Path(osdata['tarballpath'])
+def cleanup(workingDirectory, mounts):
+    for m in mounts:
+        bash(f'sudo umount {m}')
+    shutil.rmtree(workingDirectory)
 
-    # Install openelec, grub menu entry
-    if not ostarball.exists():
-        dlFile(osdata['tarballurl'],ostarball)
-        
-    shutil.copy(ostarball,workingDirectory)
-    cwd=os.getcwd()
+try:
+
+    workingDirectory=Path(tempfile.mkdtemp())
+    mounts=[]
+
+    oldcwd=os.getcwd()
     os.chdir(workingDirectory)
-    print('CWD is: {}'.format(bash('pwd').stdout))
-    bash('gunzip -c {} > image'.format(ostarball))
-    offset=getFirstPartitionOffset(Path('image'))
-    bash('mkdir mnt')
-    bash('sudo mount -o loop,offset={} ./image ./mnt'.format(offset))
-    systemPartition=disk.getPartitionByFsLabel(osdata['partition']['system']['fslabel'])
-    shutil.copytree('./mnt',systemPartition.getMountpoint())
-    os.chdir(cwd)
+
+    for osid in config['os'].keys():
+
+        osdata=config['os'][osid]
+        ostarball=Path(osdata['tarballpath'])
+
+        zipImageFile=ostarball.parts[-1]
+        imageFile=ostarball.stem
+
+        # Install openelec, grub menu entry
+        if not ostarball.exists():
+            print(f'Retrieving {osid} image')
+            dlFile(osdata['tarballurl'],ostarball)
+            
+        shutil.copy(ostarball,workingDirectory)
+
+        print(f'Extracting {osid} image')
+        bash(f'gunzip {zipImageFile}')
+
+        print(f'Mounting {osid} image')
+        offset=getFirstPartitionOffset(imageFile)
+        mountpoint=Path(workingDirectory,imageFile+'.mnt')
+        mounts.append(mountpoint)
+        bash(f'mkdir {mountpoint}')
+        bash(f'sudo mount -o loop,offset={offset} {imageFile} {mountpoint}')
+        
+        systemPartition=disk.partitiontable.getPartitionBy(osdata['partition']['system']['partitionlabel'],'partlabel')
+        print(f'Transferring files in mounted {osid} image to {systemPartition}')
+
+        # Viashell for explansion of glob
+        bash(f'sudo cp -r {mountpoint}/* {systemPartition.getMountpoint()}',viashell=True)
+        
+        # Add bootmenu entry
+        for menuItem in osdata['bootloader']:
+            for menuName,menuData in menuItem.items():
+                disk.addGrubMenuEntry(
+                    menuName,
+                    systemPartition,
+                    menuData['kpath'],
+                    menuData['karg'],
+                    menuData['class']
+                    )
+
+        
+except Exception as e:
+    cleanup(workingDirectory,mounts )
+    raise e
+
+finally:
+    cleanup(workingDirectory,mounts )
     
 # REgenerate boot menu
+disk.updateGrubMenu()
 
 
